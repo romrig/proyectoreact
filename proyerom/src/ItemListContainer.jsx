0a796dc406fbff1220abe58c45c328b4863f8be4
romrig <romansitorigali@gmail.com>
2022-11-01 19:14:32 -0300

    twocommit

---
+++ proyerom/src/ItemListContainer.jsx
@@ -0,0 +1,10 @@
+const ItemListContainer = () => {
+    return(
+    <ul className="List">
+        <li><a href="">Inicio</a></li>
+        <li><a href="">Contactanos</a></li>
+        <li><a href="">Tienda</a></li>
+    </ul>
+    );
+}
+export default ItemListContainer
